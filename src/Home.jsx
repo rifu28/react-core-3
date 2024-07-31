ebc2095d615f42fdb64cf3fee1b879820f7aa764
rifu28 <rifatmeumeu28@gmail.com>
2024-07-31 18:52:56 +0600

    second

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+    return (
+        <div>
+            <h2 className="bg-orange-400 w-1/2 mx-auto p-5 my-5 text-center text-white font-semibold text-2xl">This is home</h2>
+            <div className="flex justify-center items-center">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto temporibus odio reprehenderit, a, nemo possimus provident voluptatum suscipit error deleniti ipsum assumenda aperiam officia esse distinctio earum ullam. Dolores consectetur corporis velit illum facilis nulla placeat necessitatibus porro, voluptates nam aliquam eligendi, provident ipsum ullam reiciendis iure quibusdam aspernatur. Excepturi!</p>
+
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat deserunt minus fugiat odio ipsam ad neque et ab dolorem quidem expedita inventore, adipisci eos iusto magni voluptas accusantium mollitia! Ducimus officia ab nostrum! Esse, quis cum? Ad voluptatem eveniet excepturi odit adipisci deserunt libero, alias molestias reiciendis aperiam perspiciatis nostrum.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,18 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Root from './Root';
+import Home from './Home';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children:[
+      {
+        path: "/",
+        element: <Home></Home>
+      }
+    ]
   },
 ]);
 
