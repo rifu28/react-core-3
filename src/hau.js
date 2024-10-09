4425497f40a025629b91f6d46bafd1f6ba9c1aee
rifu28 <rifatmeumeu28@gmail.com>
2024-10-09 06:22:17 +0600

    price

---
+++ src/hau.js
@@ -0,0 +1,13 @@
+const products = [
+  { name: "apple", price: 23000 },
+  { name: "apple", price: 23000 },
+  { name: "apple", price: 23000 },
+];
+const nam = products.map((product) => {
+  product.name;
+});
+const prices = products.map((product) => {
+  product.price;
+});
+console.log(nam);
+console.log(prices);
